78a33e39de95b1ce600498b6dad59893e1eb1ba9
Tristan264 <tristan.bschwenk@icloud.com>
2021-05-21 17:01:34 +0200

    Display API URL at launch

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"goyave.dev/template/http/route"
 	_ "goyave.dev/template/http/validation"
 
 	"goyave.dev/goyave/v3"
+	"goyave.dev/goyave/v3/config"
 	_ "goyave.dev/goyave/v3/database/dialect/sqlite"
+	// _ "database/model"
 )
 
 func main() {
+	config.Load()
 	// This is the entry point of your application.
+	fmt.Printf("API Launched at %s://%s:%d", config.Get("server.protocol"), config.Get("server.host"), config.Get("server.port"))
+
 	if err := goyave.Start(route.Register); err != nil {
 		os.Exit(err.(*goyave.Error).ExitCode)
 	}
+
 }

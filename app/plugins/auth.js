3482bd04c7b4f7009a3cc3b9238fc421444ced5d
Zao Soula <contact@zaosoula.fr>
2021-06-29 12:09:34 +0200

    Fix (app/auth/github): setStrategy

--- app/plugins/auth.js
+++ app/plugins/auth.js
@@ -1,5 +1,4 @@
-export default async function ({$auth}) {
-  console.log($auth)
+export default async function ({$auth, $axios}) {
   if (!$auth.loggedIn) {
     return
   }
@@ -9,16 +8,17 @@ export default async function ({$auth}) {
     const token = $auth.getStrategy().token.get().substr(7);
     const user = $auth.user;
 
+    console.log('Request Github account');
+    
     try {
-      const {accessToken, refreshToken} = await app.$axios.$post('api/auth/github', {
+      const data = await $axios.$post('api/auth/github', {
         token,
-        user
+        ...user
       });
       
-      $auth.strategies.local.setUserToken(accessToken, refreshToken)
-      setTimeout( async () => {
-        auth.setStrategy('local');
-      });
+      $auth.setStrategy('local');
+      $auth.strategies.local.setUserToken(data.accessToken, data.refreshToken);
+      $auth.setUser(data.user);
     } catch (e) {
       console.log(e);
     }
